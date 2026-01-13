62d9af0beb52371a655111b7f8dfabdbcef6c142
iqbaltaufik11 <iqbaltaufik11@gmail.com>
2026-01-14 00:07:56 +0700

    fix 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
+"use client";
+
+import { useState } from "react";
 import ConnectWalletButton from "../src/components/ConnectWalletButton";
 import IntroModal from "../src/components/IntroModal";
 import GameCanvas from "../src/components/GameCanvas.js";
 
 export default function Home() {
+  const [introCompleted, setIntroCompleted] = useState(false);
+
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center bg-gray-900 p-4">
-      <IntroModal />
+      {!introCompleted ? (
+        <IntroModal onComplete={() => setIntroCompleted(true)} />
+      ) : (
+        <>
+          <ConnectWalletButton style={{ marginBottom: 16 }} />
 
-      <ConnectWalletButton style={{ marginBottom: 16 }} />
-
-      <div className="border-4 border-white shadow-lg shadow-purple-500/50">
-        <GameCanvas width={800} height={600} />
-      </div>
+          <div className="border-4 border-white shadow-lg shadow-purple-500/50">
+            <GameCanvas width={800} height={600} />
+          </div>
+        </>
+      )}
     </main>
   );
 }
